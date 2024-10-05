42e837d992eb6c3c18d5d6327a71e40ad1882299
Savvas Lavasas <s_lavas@hotmail.com>
2024-10-06 00:57:38 +0200

    update latest files

--- application/learningapp/obj/Debug/net8.0/learningapp.AssemblyInfo.cs
+++ application/learningapp/obj/Debug/net8.0/learningapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70d431e5f840d2532b74418ee55bcd6376550bc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea28ad5dc5672aa200700231dc176b38b4c8b007")]
 [assembly: System.Reflection.AssemblyProductAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
